0c6e4e51d2cfc36c5cd868e7c4942aea3a9784c7
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-16 18:11:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function calculateMinCost() {
   //your code here
-  
+     var input = document.getElementById("rope-lengths").value;
+      var arr = input.split(",").map(Number);
+      arr.sort(function(a, b) {
+        return a - b;
+      });
+      var cost = 0;
+      var totalCost = 0;
+    for (var i = 0; i < arr.length; i++)
+	 {
+        cost = arr[i] + arr[i + 1];
+        totalCost += cost;
+        arr[i + 1] = cost;
+        arr.sort(function(a, b) {
+          return a - b;
+        });
+	 }
+      document.getElementById("result").innerHTML =totalCost;
   
   
 }  
